71328adb1a1087070e99f7daea4ba9dffe77d2a8
DDublue <theonly1living@gmail.com>
2025-04-26 01:55:28 -0700

    Forgot to adjust signIn of authClient for Spotify

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,9 @@
 import {createAuthClient} from 'better-auth/react';
 
-export const authClient = createAuthClient({
-  baseURL: 'http://localhost:3000',
-  // other client config
-});
+export const authClient = createAuthClient();
+
+export const signIn = async () => {
+  const data = await authClient.signIn.social({
+    provider: 'spotify'
+  });
+};
